eb5e7e6c5a41390029afeafd7e978772c0fc6ac0
Gautam Krishna B <gautamkrishna0007@gmail.com>
2026-08-29 00:08:42 +0530

    feat(session_1): add basics class problems

---
+++ src/main/java/basics/class_problems/PrimeNUmberChecker.java
@@ -0,0 +1,23 @@
+package basics.class_problems;
+
+import java.util.Scanner;
+
+public class PrimeNUmberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        boolean IsPrime = true;
+        if (number <= 1) {
+            IsPrime = false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    IsPrime = false;
+                    break;
+                }
+            }
+        }
+        System.out.println("Is it prime? " + IsPrime);
+    }
+}
